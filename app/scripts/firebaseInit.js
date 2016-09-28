79e482c6f882b938645d1d95b2bc8d6b03b9994c
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-09-28 10:03:57 -0400

    OS Start Bloc Chat

---
+++ app/scripts/firebaseInit.js
@@ -0,0 +1,10 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA1SQvbJjRluvE8txGWP8CZkdx-BGFdvS8",
+    authDomain: "bloc-chat-a8ca5.firebaseapp.com",
+    databaseURL: "https://bloc-chat-a8ca5.firebaseio.com",
+    storageBucket: "bloc-chat-a8ca5.appspot.com",
+    messagingSenderId: "608655752639"
+};
+firebase.initializeApp(config);
